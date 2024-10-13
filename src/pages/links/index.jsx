a232b229d477616d461ca9691d7d0f3331c640c7
dutra-pc <joao.vitornl@gmail.com>
2024-10-12 23:49:43 -0300

    :construction: Create links page

---
+++ src/pages/links/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Box } from './styles';
+
+const Links = () => {
+
+    return (
+        <Container>
+            <Box>
+                <p>links</p>
+            </Box>
+        </Container>
+    )
+}
+
+export default Links
